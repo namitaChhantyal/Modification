2fb4cd11cd5fe1de7d1097ea8090879cdf390f97
Namita Chhantyal <chhantyalnamita111@gmail.com>
2020-02-13 17:49:11 +0545

    first commit

--- GarbageP/pages/urls.py
+++ GarbageP/pages/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index'),
-    path('about',views.about,name='about')
+    path('home',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('login',views.login,name='login'),
+    path('payment',views.payment,name='payment')
+
+
 
 ]
--- GarbageP/pages/views.py
+++ GarbageP/pages/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Page
 
-def index(request):
-    return render(request,'index.html')
+def home(request):
+    return render(request,'home.html')
 
 def about(request):
-    return HttpResponse('about')
+    return render(request,'about.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def payment(request):
+    return render(request,'payment.html')
